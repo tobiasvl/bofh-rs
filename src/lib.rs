2d739ca894f41df8909cec58b90a0c6b8c936b7f
Tobias V. Langhoff <tobias@langhoff.no>
2022-06-14 00:12:49 +0200

    Minimal working bofh client
    Connects, requests MOTD and logs in

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,144 @@
+use thiserror::Error;
+use xmlrpc::{Request, Value};
+
+#[derive(Error, Debug)]
+pub enum BofhError {
+    #[error("{0}")]
+    XmlRpcError(#[from] xmlrpc::Error),
+    #[error("Attempted to run session command before session was established")]
+    NoSessionError,
+    #[error("{0}")]
+    CerebrumError(String),
+    #[error("Server restarted")]
+    ServerRestartedError,
+    #[error("Session expired")]
+    SessionExpiredError,
+    #[error("Unknown command")]
+    NotImplementedError,
+    #[error("{0}")]
+    Fault(String),
+}
+
+pub struct Bofh {
+    /// The URL to the bofhd server
+    pub url: String,
+    session: Option<String>,
+    /// The Message Of The Day provided by the bofhd server after connection
+    pub motd: Option<String>,
+}
+
+impl Bofh {
+    pub fn new(url: String) -> Result<Self, BofhError> {
+        let mut bofh = Self {
+            url,
+            session: None,
+            motd: None,
+        };
+        bofh.motd = Some(bofh.get_motd()?);
+        Ok(bofh)
+    }
+
+    fn run_request(&mut self, request: Request) -> Result<Value, BofhError> {
+        match request.call_url(&self.url) {
+            Ok(result) => Ok(result),
+            Err(err) => {
+                if let Some(fault) = err.fault() {
+                    if let Some(bofhd_error) = fault
+                        .fault_string
+                        .strip_prefix("Cerebrum.modules.bofhd.errors.")
+                    {
+                        if let Some(cerebrum_error) = bofhd_error.strip_prefix("CerebrumError:") {
+                            Err(BofhError::CerebrumError(cerebrum_error.to_string()))
+                        } else if bofhd_error.strip_prefix("ServerRestartedError:").is_some() {
+                            //Err(BofhError::ServerRestartedError)
+                            //self.init_commands(True);
+                            self.run_request(request)
+                        } else if bofhd_error.strip_prefix("SessionExpiredError:").is_some() {
+                            //Err(BofhError::SessionExpiredError(request))
+                            todo!() // TODO
+                        } else {
+                            unimplemented!()
+                        }
+                    } else if fault
+                        .fault_string
+                        .strip_prefix("NotImplementedError:")
+                        .is_some()
+                    {
+                        Err(BofhError::NotImplementedError)
+                    } else {
+                        Err(BofhError::Fault(fault.fault_string.to_owned()))
+                    }
+                } else {
+                    Err(BofhError::XmlRpcError(err))
+                }
+            }
+        }
+    }
+
+    fn run_raw_command(&mut self, command: &str, args: Vec<&str>) -> Result<Value, BofhError> {
+        let mut request = Request::new(command);
+        for arg in args {
+            request = request.arg(arg);
+        }
+        self.run_request(request)
+    }
+
+    fn run_raw_sess_command(&mut self, command: &str, args: Vec<&str>) -> Result<Value, BofhError> {
+        if let Some(session) = &self.session {
+            let mut request = Request::new(command).arg(session.to_owned());
+            for arg in args {
+                request = request.arg(arg);
+            }
+            self.run_request(request)
+        } else {
+            // TODO Maybe just panic here instead, this should never happen
+            Err(BofhError::NoSessionError)
+        }
+    }
+
+    pub fn run_command(&mut self, command: &str, args: Vec<&str>) -> Result<Value, BofhError> {
+        self.run_raw_sess_command(command, args)
+    }
+
+    pub fn login(
+        &mut self,
+        username: &str,
+        password: String,
+        _init: bool,
+    ) -> Result<(), BofhError> {
+        self.session = Some(
+            self.run_raw_command("login", vec![username, &password])?
+                .as_str()
+                .unwrap()
+                .to_string(),
+        );
+        Ok(())
+    }
+
+    pub fn get_motd(&mut self) -> Result<String, BofhError> {
+        Ok(self
+            .run_raw_command("get_motd", vec![])?
+            .as_str()
+            .unwrap()
+            .to_string())
+    }
+}
+
+impl Drop for Bofh {
+    fn drop(&mut self) {
+        if self.session.is_some() {
+            let _ = self.run_raw_sess_command("logout", vec![]);
+        }
+        self.session = None;
+        // TODO bring down all commands
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::Bofh;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn connect() {
+        let _ = Bofh::new(String::from("https://cerebrum-uio-test.uio.no:8000"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
+use bofh::Bofh;
+use clap::Parser;
+use rpassword::prompt_password;
+use rustyline::{error::ReadlineError, Editor};
+
+/// The Cerebrum Bofh client
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    ///// Run command and exit
+    //#[clap(long, required = false)]
+    //cmd: String,
+    /// connect to bofhd server at URL
+    #[clap(long, default_value_t = String::from("https://cerebrum-uio-test.uio.no:8000/"))]
+    url: String,
+
+    /// authenticate as USER
+    #[clap(long, short, default_value_t = whoami::username())]
+    user: String,
+
+    /// skip certificate hostname validation
+    #[clap(long)]
+    insecure: bool,
+
+    /// set connection timeout to N seconds
+    #[clap(long, default_value_t = 0, value_name = "N")]
+    timeout: u8,
+
+    /// use vi command mode (cheatsheet:
+    /// https://catonmat.net/ftp/bash-vi-editing-mode-cheat-sheet.pdf)
+    #[clap(long, alias = "vim")]
+    vi: bool,
+
+    /// use a custom prompt
+    #[clap(long, short, default_value_t = String::from("bofh> "))]
+    prompt: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Connecting to {}\n", &args.url);
+    let mut bofh = match Bofh::new(args.url) {
+        Ok(bofh) => bofh,
+        Err(err) => {
+            println!("{}", err);
+            std::process::exit(1);
+        }
+    };
+
+    if let Some(motd) = &bofh.motd {
+        println!("{}\n", motd);
+    }
+
+    let password = match prompt_password(format!("Password for {}: ", &args.user)) {
+        Ok(password) => password,
+        Err(_) => std::process::exit(0),
+    };
+
+    if let Err(err) = bofh.login(&args.user, password, true) {
+        println!("{}", err);
+        std::process::exit(1);
+    };
+
+    let mut rl = Editor::<()>::new();
+
+    //if args.vi {
+    //    rl.set_edit_mode(rustyline::EditMode::Vi)
+    //}
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        match rl.readline(&args.prompt) {
+            Ok(line) => {
+                println!("{:?}", bofh.run_command(&line, vec![]));
+                rl.add_history_entry(&line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    println!("So long, and thanks for all the fish!");
+    rl.append_history("history.txt").unwrap();
 }
